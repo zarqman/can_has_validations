692726022bd8ec4181b698714539ce48716ccd6c
thomas morgan <tm@iprog.com>
2013-06-05 13:39:46 -0600

    minor tweak

--- lib/can_has_validations/validators/email_validator.rb
+++ lib/can_has_validations/validators/email_validator.rb
@@ -3,8 +3,11 @@
 
 module ActiveModel::Validations
   class EmailValidator < ActiveModel::EachValidator
+
+    EMAIL_REGEXP = /\A([a-z0-9._+-]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
     def validate_each(record, attribute, value)
-      unless value =~ /\A([a-z0-9._+-]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+      unless value =~ EMAIL_REGEXP
         record.errors.add(attribute, :invalid_email, options)
       end
     end
