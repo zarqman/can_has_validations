e768fb9a087c11e229785cd3449e71af7ec3d649
thomas morgan <tm@iprog.com>
2018-05-18 16:05:11 -0600

    grandparent: allow _id form of attribute

--- lib/can_has_validations/validators/grandparent_validator.rb
+++ lib/can_has_validations/validators/grandparent_validator.rb
@@ -2,11 +2,15 @@
 #   :allow_nil will not only allow the attribute/association to be nil, but
 #     also any of the :scope's.
 # eg: validates :user, grandparent: {scope: :org, parent: :realm}
-#     validates :user, grandparent: {scope: [:phone, :address], parent: :account_id}
+#     validates :user_id, grandparent: {scope: [:phone, :address], parent: :account_id}
 
 module ActiveModel::Validations
   class GrandparentValidator < ActiveModel::EachValidator
     def validate_each(record, attribute, association)
+      # to allow attribute to be the _id and not just the actual association
+      if attribute.to_s.ends_with?('_id')
+        association = record.send(attribute.to_s.sub(/_id$/,''))
+      end
       all_match = Array(options[:scope]).all? do |scope|
         cousin = record.send(scope)
         if cousin.nil?
